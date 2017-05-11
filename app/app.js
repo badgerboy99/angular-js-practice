22086c770efce5df06c19f4798e612eea1b58ed9
Chris <releasethebadgers@yahoo.co.uk>
2017-05-11 12:41:14 +0100

    Added 'Show' and 'Click'

--- app/app.js
+++ app/app.js
@@ -12,27 +12,36 @@ myFoodApp.controller('foodController', ['$scope', function($scope){
 
 	$scope.message = "It's alive!";
 
+	$scope.removeFood = function(food){
+		var removedFood = $scope.foods.indexOf(food);
+		$scope.foods.splice(removedFood, 1)
+	}
+
 	$scope.foods = [
 
 		{
 			name: "Aubergine",
 			colour: 'purple',
-			kg: 2.4
+			kg: 2.4,
+			available: true
 		},
 		{
 			name: 'Kumquat',
 			colour: 'orange',
-			kg: 2.95
+			kg: 2.95,
+			available: true
 		},
 		{
 			name: 'Artichoke',
 			colour: 'green',
-			kg: 3.89
+			kg: 3.89,
+			available: true
 		},
 		{
 			name: 'Pomegranate',
-			colour: 'dark red',
-			kg: 3.6
+			colour: 'red',
+			kg: 3.6,
+			available: true
 		},
 	];
 		
