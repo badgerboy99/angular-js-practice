9880d209023248a95be444d2f735be3fea92fcff
Chris <releasethebadgers@yahoo.co.uk>
2017-05-11 10:23:40 +0100

    Added filters

--- app/app.js
+++ app/app.js
@@ -8,10 +8,33 @@ var myFoodApp = angular.module('myFoodApp', []);
 
 //});
 
-myFoodApp.controller('foodController', [function($scope){
+myFoodApp.controller('foodController', ['$scope', function($scope){
 
 	$scope.message = "It's alive!";
 
-	$scope.foods = ['aubergine', 'kumquat', 'artichoke', 'pomegranate'];
+	$scope.foods = [
+
+		{
+			name: "Aubergine",
+			colour: 'purple',
+			kg: 2.4
+		},
+		{
+			name: 'Kumquat',
+			colour: 'orange',
+			kg: 2.95
+		},
+		{
+			name: 'Artichoke',
+			colour: 'green',
+			kg: 3.89
+		},
+		{
+			name: 'Pomegranate',
+			colour: 'dark red',
+			kg: 3.6
+		},
+	];
+		
 
 }]);
